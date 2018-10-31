58c2dd13b60cf4f6bf629a486dfac43b8c1bed32
Tim Joosten <Tjoosten3@gmail.com>
2018-10-31 08:45:09 +0100

    feat: Implement seeder command for db refresh
    If the environment is not prod of production we throw the question
    if the developer or contributor wants to reset the database or not.
    
    [SIGNED OFF]: Tim Joosten <topairy@gmail.com>

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -2,6 +2,9 @@
 
 use Illuminate\Database\Seeder;
 
+/**
+ * Class DatabaseSeeder
+ */
 class DatabaseSeeder extends Seeder
 {
     /**
@@ -10,7 +13,25 @@ class DatabaseSeeder extends Seeder
      * @return void
      */
     public function run(): void
-    {
+    { 
+        if ($this->resetDatabase()) {
+            $this->command->call('migrate:refresh');
+            $this->command->warn('Data cleared, starting from blank database.');
+        }
+
+
+        // Run other seeds in the application. 
         $this->call(CityScaffoldingSeeder::class);
+    }
+
+    /**
+     * Function for determining if we need to reset the database storage or not. 
+     * 
+     * @return bool
+     */
+    protected function resetDatabase(): bool 
+    {
+        return ! app()->environment(['production', 'prod']) 
+            && $this->command->confirm('Do you wish to refresh migrations before seeding, it will clear all old data?');
     }
 }
