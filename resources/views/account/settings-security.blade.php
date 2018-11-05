0f036e7995af6a0e11fd00e42b97ebcf7d65f92c
Tim Joosten <Tjoosten3@gmail.com>
2018-11-05 15:55:36 +0100

    Implement security settings view

--- resources/views/account/settings-security.blade.php
+++ resources/views/account/settings-security.blade.php
@@ -16,7 +16,28 @@
 
             <div class="col-md-9">
                 <form class="card card-body" method="POST" action="">
-                    @csrf {{-- Form field protection --}}
+                    @include ('flash::message') {{-- Flash session view partial --}}
+
+                    @csrf               {{-- Form field protection --}}
+                    @method('PATCH')    {{-- HTTP method spoofing --}}
+
+                     <div class="form-group">
+                        <label for="inputPassword">New password <span class="text-danger">*</span></label>
+                        <input type="password" class="form-control @error('password', 'is-invalid')" placeholder="Your new password" @input('password')>
+                        @error('password')
+                    </div>
+
+                    <div class="form-group">
+                        <label for="passwordConfirmation">Repeat password <span class="text-danger">*</span></label>
+                        <input type="password" @input('password_confirmation') class="form-control" placeholder="Repeat password">
+                    </div>
+
+                    <hr class="mt-0 border-grey">
+
+                    <div class="form-group mb-0">
+                        <button type="submit" class="btn btn-success rounded">Update</button>
+                        <button type="reset" class="btn btn-link rounded">Reset</button>
+                    </div>
                 </form>
             </div>
         </div>
