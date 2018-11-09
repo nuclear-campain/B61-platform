3e184ce6809abd363bda966bbe658fe279f629e8
Tim Joosten <Tjoosten3@gmail.com>
2018-11-09 13:20:23 +0100

    Work on the views

--- resources/views/articles/show.blade.php
+++ resources/views/articles/show.blade.php
@@ -28,7 +28,7 @@
                     <h5 class="border-bottom border-gray pb-1 mb-2">{{ ucfirst($article->title) }}</h5>
                     <small class="align-middle mb-2 text-secondary">
                         Posted {{ $article->created_at->diffForHumans() }} by {{ $article->author->name }}
-                        <a href="#" class="no-underline float-right">
+                        <a href="#comments" class="no-underline float-right">
                             <i class="fe fe-message-square"></i> Comments (3)
                         </a>
                     </small>
@@ -54,6 +54,59 @@
                             </span>
                         </div>
                     </div>
+                </div>
+            </div>
+
+            <div class="col-md-8">
+                <div class="card card-body mb-3">
+                    <h6 class="border-bottom border-gray pb-1 mb-2"><a id="comments"></a> 0 reacties</h6>
+
+                    {{-- <p class="card-text text-secondary mb-1">There are no comments at this time.</p> --}}
+
+                    <div class="media text-muted pt-2">
+                        <img data-src="holder.js/32x32?theme=thumb&amp;bg=007bff&amp;fg=007bff&amp;size=1" alt="32x32" class="mr-2 rounded" src="data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%2232%22%20height%3D%2232%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%2032%2032%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_166f84f3ee7%20text%20%7B%20fill%3A%23007bff%3Bfont-weight%3Abold%3Bfont-family%3AArial%2C%20Helvetica%2C%20Open%20Sans%2C%20sans-serif%2C%20monospace%3Bfont-size%3A2pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_166f84f3ee7%22%3E%3Crect%20width%3D%2232%22%20height%3D%2232%22%20fill%3D%22%23007bff%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%2211.546875%22%20y%3D%2216.9%22%3E32x32%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E" data-holder-rendered="true" style="width: 32px; height: 32px;">
+          
+                        <div class="media-body pb-2 mb-0 small lh-125 border-bottom border-gray">
+                            <div class="d-flex justify-content-between w-100">
+                                <div class="align-items-center">
+                                    <strong class="text-gray-dark">Full Name</strong> <small>13 seconds ago</small>
+                                </div>
+              
+                                <div class="align-items-right">
+                                    <a href="#" class="text-secondary no-underline"><i class="fe fe-thumbs-up"></i></a> <span class="text-secondary mr-1">0</span>
+                                    <a href="#" class="text-secondary no-underline"><i class="fe fe-thumbs-down"></i></a> <span class="text-secondary mr-1">0</span>
+                                    <a href="#" class="text-danger no-underline"><i class="fe fe-alert-octagon"></i></a>
+                                </div>
+                            </div>
+                            
+                            <span class="d-block">comment description</span>
+                        </div>
+                    </div>
+
+                    <small class="d-block text-right mt-2">
+                        <a href="#">All updates</a>
+                        <a href="#">All updates</a>
+                    </small>
+
+                    <hr class="mt-2 mb-2">
+                       
+                    @auth
+                        <form class=""> {{-- Comment form --}}
+                            <div class="form-group">
+                                <textarea class="form-control" placeholder="Your comment" id="exampleFormControlTextarea1" rows="3"></textarea>
+                            </div>
+
+                            <div class="form-group mb-0">
+                                <button class="btn btn-sm btn-primary">
+                                    <i class="fe fe-message-square mr-1"></i> Comment
+                                </button>
+                            </div>
+                        </form> {{-- /// END comment form --}}
+                    @else
+                        <div class="alert alert-info alert-important mb-0" role="alert">
+                            Please <a href=""><strong>sign in</strong></a> or <a href=""><strong>create an account</strong></a> to participate in this conversation.
+                        </div>
+                    @endauth
                 </div>
             </div>
 
