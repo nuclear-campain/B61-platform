60fae1ae912ef216085f7cd1320a62f4664e4de5
Tim Joosten <Tjoosten3@gmail.com>
2019-01-30 18:29:12 +0100

    Update

--- resources/views/articles/show.blade.php
+++ resources/views/articles/show.blade.php
@@ -60,6 +60,7 @@
             <div class="col-md-8"> {{-- Comment section --}}
                 <div class="card card-body mb-3">
                     <h6 class="border-bottom border-gray pb-1 mb-2"><a id="comments"></a>{{ $commentsCount }} Comments</h6>
+                    @include ('flash::message') {{-- Flash session view partial --}}
 
                     @forelse ($comments as $comment) {{-- Loop through the comments --}}
                         <div class="media small text-muted pt-2">
--- resources/views/comments/edit.blade.php
+++ resources/views/comments/edit.blade.php
@@ -24,6 +24,8 @@
         <div class="row">
             <div class="col-md-9"> {{-- EDIT BOX --}}
                 <form method="POST" action="" class="card card-body">
+                    @csrf {{-- Form field protection --}}
+                    
                     <h6 class="border-bottom border-gray pb-1 mb-2">Edit comment</h6>
                 </form> {{-- /// END edit box --}}
             </div>
