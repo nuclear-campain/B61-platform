36d8eb1e30e050801cf917859ce378344e40713f
Tim Joosten <Tjoosten3@gmail.com>
2019-02-03 20:48:12 +0100

    implement class docs

--- app/Http/Controllers/Petition/IndexController.php
+++ app/Http/Controllers/Petition/IndexController.php
@@ -5,6 +5,11 @@ namespace App\Http\Controllers\Petition;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
+/**
+ * Class IndexController
+ * 
+ * @package App\Http\Controllers\Petition
+ */
 class IndexController extends Controller
 {
     //
--- app/Http/Controllers/Petition/SignatureController.php
+++ app/Http/Controllers/Petition/SignatureController.php
@@ -5,6 +5,11 @@ namespace App\Http\Controllers\Petition;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
+/**
+ * Class SignatureController 
+ * 
+ * @package App\Http\Controllers\Petition
+ */
 class SignatureController extends Controller
 {
     //
