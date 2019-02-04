44da364fe42e5a5b013b002eb3c4c31f69657fb1
Tjoosten <Topairy@gmail.com>
2019-02-04 18:19:36 +0100

    Complete signature table.

--- app/Models/Signature.php
+++ app/Models/Signature.php
@@ -3,8 +3,30 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
+/**
+ * Class Signature
+ *
+ * @package App\Models
+ */
 class Signature extends Model
 {
-    //
+    /**
+     * Mass-assign fields for the database table.
+     *
+     * @var array
+     */
+    protected $fillable = ['firstname', 'lastname', 'email', 'city', 'country'];
+
+    /**
+     * Data relation for getting the city information in the application.
+     *
+     * @return BelongsTo
+     */
+    public function cityInformation(): BelongsTo
+    {
+        return $this->belongsTo(City::class, 'city')
+            ->withDefault(['name' => 'Stad onbekend.']);
+    }
 }
